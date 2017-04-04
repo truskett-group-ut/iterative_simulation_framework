f64bef94ee50b2c9aba94a15aa1f07302271ae32
rjadrich <rjadrich@gmail.com>
2017-04-03 22:44:39 -0500

    updated setting file format and the data needed by the main iterative script

--- interface/generate_update.py
+++ interface/generate_update.py
@@ -2,51 +2,49 @@ import json
 import sys
 import re
 
-with open('../settings.xml', 'r') as outfile:  
-    path = re.search(r'<code_directory>(.+)</code_directory>', outfile.read()).group(1)
+def GenerateUpdate():
+    from xml_extractor import xml_extractor 
+    from potentials import potential_combiner
 
-sys.path.insert(0, path)
+    #read in the initialization data
+    with open('../potential_specs__params_state.json') as data_file:    
+        potential_specs__params_state = json.load(data_file)
+        potential_specs = potential_specs__params_state['specs']
+        params_state = potential_specs__params_state['state']
 
-from xml_extractor import xml_extractor 
-from potentials import potential_combiner
+    #create the potential
+    potential = potential_combiner.Potential(potential_specs)
+    potential.SetParamsState(params_state)
 
-#read in the initialization data
-with open('../potential_specs__params_state.json') as data_file:    
-    potential_specs__params_state = json.load(data_file)
-    potential_specs = potential_specs__params_state['specs']
-    params_state = potential_specs__params_state['state']
-    
-#create the potential
-potential = potential_combiner.Potential(potential_specs)
-potential.SetParamsState(params_state)
+    #read in potential parameter values
+    with open('./params_val.json') as data_file:    
+        params_val = json.load(data_file)
 
-#read in potential parameter values
-with open('./params_val.json') as data_file:    
-    params_val = json.load(data_file)
+    #load in xml settings
+    xml_extractor = xml_extractor.XMLExtractor()
+    xml_extractor.Parse('../settings.xml')
 
-#load in xml settings
-xml_extractor = xml_extractor.XMLExtractor()
-xml_extractor.Parse('../settings.xml')
+    ###########################################################################################
 
-###########################################################################################
+    optimization_type = xml_extractor.GetText('optimization', 'type').strip().lower()
+    if optimization_type == 'relative_entropy':
+        from relative_entropy_optimizer import relative_entropy_update
+        xmlParser = lambda x: xml_extractor.GetText('optimization', 'relative_entropy', x)
 
-optimization_type = xml_extractor.GetText('optimization', 'type').strip().lower()
-if optimization_type == 'relative_entropy':
-    from relative_entropy_optimizer import relative_entropy_update
-    xmlParser = lambda x: xml_extractor.GetText('optimization', 'relative_entropy', x)
+        #perform relative entropy update
+        relative_entropy_update = relative_entropy_update.RelativeEntropyUpdate()
+        relative_entropy_update.LoadPotential(potential, params_val)
+        relative_entropy_update.LoadRadialDistFuncs('./rdf.xvg', '../rdf_target.xvg', spacing=float(xmlParser('dr_integrate')))
+        params_val_out, conv_score = relative_entropy_update.CalcUpdate(learning_rate=float(xmlParser('learning_rate')))
+    else:
+        raise Exception('Invalid simulation program provided in settings file.')
 
-    #perform relative entropy update
-    relative_entropy_update = relative_entropy_update.RelativeEntropyUpdate()
-    relative_entropy_update.LoadPotential(potential, params_val)
-    relative_entropy_update.LoadRadialDistFuncs('./rdf.xvg', '../rdf_target.xvg', spacing=float(xmlParser('dr_integrate')))
-    params_val_out = relative_entropy_update.CalcUpdate(learning_rate=float(xmlParser('learning_rate')))
-else:
-    raise Exception('Invalid simulation program provided in settings file.')
-    
-#write out new potential parameter values
-with open('./params_val_out.json', 'w') as data_file:      
-    json.dump(params_val_out, data_file, indent=4, sort_keys=True)
-    
-#write out the old one but in sorted order
-with open('./params_val.json', 'w') as data_file:    
-    json.dump(params_val, data_file, indent=4, sort_keys=True)
+    #write out new potential parameter values
+    with open('./params_val_out.json', 'w') as data_file:      
+        json.dump(params_val_out, data_file, indent=4, sort_keys=True)
+
+    #write out the old one but in sorted order
+    with open('./params_val.json', 'w') as data_file:    
+        json.dump(params_val, data_file, indent=4, sort_keys=True)
+        
+    return conv_score
--- interface/prepare_simulation.py
+++ interface/prepare_simulation.py
@@ -2,34 +2,6 @@ import json
 import sys
 import re
 
-with open('../settings.xml', 'r') as outfile:  
-    path = re.search(r'<code_directory>(.+)</code_directory>', outfile.read()).group(1)
-
-sys.path.insert(0, path)
-
-from xml_extractor import xml_extractor 
-from potentials import potential_combiner
-
-#read in the initialization data
-with open('../potential_specs__params_state.json') as data_file:    
-    potential_specs__params_state = json.load(data_file)
-    potential_specs = potential_specs__params_state['specs']
-    params_state = potential_specs__params_state['state']
-    
-#create the potential
-potential = potential_combiner.Potential(potential_specs)
-potential.SetParamsState(params_state)
-
-#read in potential parameter values
-with open('./params_val.json') as data_file:    
-    params_val = json.load(data_file)
-
-#load in xml settings
-xml_extractor = xml_extractor.XMLExtractor()
-xml_extractor.Parse('../settings.xml')
-
-###########################################################################################
-
 def MakeBool(text):
     text = text.lower()
     if re.match(r'\s*true\s*', text):
@@ -37,24 +9,48 @@ def MakeBool(text):
     elif re.match(r'\s*false\s*', text):
         return False
 
-simulation_program = xml_extractor.GetText('simulation', 'program').strip().lower()
-if simulation_program == 'gromacs':
-    from gromacs_interface_tools import gromacs_potential_maker
-    xmlParser = lambda x1, x2: xml_extractor.GetText('simulation', 'gromacs', x1, x2)
+def PrepareSimulation():
+    from xml_extractor import xml_extractor 
+    from potentials import potential_combiner
 
-    #generate gromacs data
-    gromacs_potential_maker = gromacs_potential_maker.SimulationPotentialConverter()
-    gromacs_potential_maker.LoadPotential(potential, params_val)
-    gromacs_potential_maker.TabulatePotential(r_max=float(xmlParser('table', 'r_max')), 
-                                              dr=float(xmlParser('table', 'dr'))
-                                             )
-    gromacs_potential_maker.CutShiftTabulated(e_max=float(xmlParser('potential', 'e_max')), 
-                                              f_max=float(xmlParser('potential', 'f_max')), 
-                                              r_max=float(xmlParser('potential', 'r_max')), 
-                                              shift=MakeBool(xmlParser('potential', 'shift'))
-                                             )
-    gromacs_potential_maker.MakeTable(filename='./table.xvg')
-    gromacs_potential_maker.InsertGromppCutoff(r_buffer=float(xmlParser('grompp', 'r_buffer')), 
-                                               filename='./grompp.mdp')
-else:
-    raise Exception('Invalid simulation program provided in settings file.')
+    #read in the initialization data
+    with open('../potential_specs__params_state.json') as data_file:    
+        potential_specs__params_state = json.load(data_file)
+        potential_specs = potential_specs__params_state['specs']
+        params_state = potential_specs__params_state['state']
+
+    #create the potential
+    potential = potential_combiner.Potential(potential_specs)
+    potential.SetParamsState(params_state)
+
+    #read in potential parameter values
+    with open('./params_val.json') as data_file:    
+        params_val = json.load(data_file)
+
+    #load in xml settings
+    xml_extractor = xml_extractor.XMLExtractor()
+    xml_extractor.Parse('../settings.xml')
+
+    ###########################################################################################
+
+    simulation_program = xml_extractor.GetText('simulation', 'program').strip().lower()
+    if simulation_program == 'gromacs':
+        from gromacs_interface_tools import gromacs_potential_maker
+        xmlParser = lambda x1, x2: xml_extractor.GetText('simulation', x1, x2)
+
+        #generate gromacs data
+        gromacs_potential_maker = gromacs_potential_maker.SimulationPotentialConverter()
+        gromacs_potential_maker.LoadPotential(potential, params_val)
+        gromacs_potential_maker.TabulatePotential(r_max=float(xmlParser('table', 'r_max')), 
+                                                  dr=float(xmlParser('table', 'dr'))
+                                                 )
+        gromacs_potential_maker.CutShiftTabulated(e_max=float(xmlParser('potential', 'e_max')), 
+                                                  f_max=float(xmlParser('potential', 'f_max')), 
+                                                  r_max=float(xmlParser('potential', 'r_max')), 
+                                                  shift=MakeBool(xmlParser('potential', 'shift'))
+                                                 )
+        gromacs_potential_maker.MakeTable(filename='./table.xvg')
+        gromacs_potential_maker.InsertGromppCutoff(r_buffer=float(xmlParser('gromacs', 'r_buffer')), 
+                                                   filename='./grompp.mdp')
+    else:
+        raise Exception('Invalid simulation program provided in settings file.')

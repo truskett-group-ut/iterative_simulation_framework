ecad3a32d56a739d6ad9b89dfdd3530a7ab7e891
William Pineros <pineros@viking.che.utexas.edu>
2017-08-11 20:35:07 -0500

    some run gromacs control changes

--- run_gromacs.sh
+++ run_gromacs.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 #wp: input: 1. num of cores (int) 2. outconfig file from simulation
 #
+
+nt=$1
+outconfig_file=$2 
+
 if [ -f $PWD/../my_run_gromacs.sh ]; then
   $PWD/../my_run_gromacs.sh
 else
   gmx grompp -n ../index.ndx -p ../topol.top
-  #mdrun -nt $1 -x traj.xtc -g -v
-  gmx mdrun -nt $1 -x traj.xtc -g 
+  grompp_status=$?
+  #ensures grompp is done correctly else doesn't run simulation
+  if [ $grompp_status -eq 0 ]; then
+	gmx mdrun -nt $nt -x traj.xtc -g 
+	if [ -f $outconfig_file ]; then
+	  touch done.txt
+	else
+	  echo "gromacs crashed for some reason, consult the log file" 
+	  exit 
+	fi 
+  else
+	echo "Grompp failed. Check your input"
+	exit;
+  fi
 fi
-if [ -f $2 ]; then
-  touch done.txt
-else
-  echo "gromacs crashed for some reason, consult the log file" 
-fi 
-exit 
